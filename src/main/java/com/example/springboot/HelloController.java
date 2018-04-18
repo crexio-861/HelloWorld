3b6c1e46ba34d853cf36d6ed42450a2d6f732e25
guderin <gatashare@gmail.com>
2018-04-18 11:03:02 +0900

    first commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.springboot;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController // (1)
+public class HelloController {
+	@RequestMapping("/hello") // (2)
+	public String hello() {
+		return "Hello World !!";
+	}
+}
